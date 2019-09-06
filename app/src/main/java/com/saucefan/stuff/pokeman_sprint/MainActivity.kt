800abb50161910bae699b81167c41a7ac61a34f5
jack seymour <stilljack@gmail.com>
2019-09-06 08:48:20 -0700

    commit before i got to the whiteboard to plot out how i want to build this

--- app/src/main/java/com/saucefan/stuff/pokeman_sprint/MainActivity.kt
+++ app/src/main/java/com/saucefan/stuff/pokeman_sprint/MainActivity.kt
@@ -3,10 +3,54 @@ package com.saucefan.stuff.pokeman_sprint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+                /*
+                *
+                * initial scouting of api
+                * BASE_URL ="https://pokeapi.co/api/v2/"
+                * https://pokeapi.co/api/v2/pokemon-species/[id]/ will give us our particular pokemons details
+                * https://pokeapi.co/api/v2/pokedex/[regional pokedex]/ gives us the list of pokemon for a region
+                *
+                * I'M JUST GONNA DO REGION TWO INITIALLY, KANTO OR THE ORIGINAL 150 POKEMON
+                *
+                * postman tells me:
+                *
+                * should be able to build a list out of pokedex/2 without a terrible amount of trouble
+                * pokedex/{id} is a - ok for getting everything we need for details view
+                *
+                * trailing / is uncessary
+                * ~~ whiteboard and try to figure out everything that needs to get built
+                *
+                *
+                *
+                *
+                *
+                *
+                *
+                *
+                *
+                *
+                * */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
